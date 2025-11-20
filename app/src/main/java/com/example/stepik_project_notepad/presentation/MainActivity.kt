cf491a0ba8f675ea0ecf84c1ab0b701646f94b06
Mark <markdoroshko@yandex.ru>
2025-11-20 17:48:23 +0300

    RememberScrollState.

--- app/src/main/java/com/example/stepik_project_notepad/presentation/MainActivity.kt
+++ app/src/main/java/com/example/stepik_project_notepad/presentation/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.stepik_project_notepad.presentation.screens.notes.NotesScreen
 import com.example.stepik_project_notepad.presentation.ui.theme.StepikProjectNotePadTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StepikProjectNotePadTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NotesScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StepikProjectNotePadTheme {
-        Greeting("Android")
     }
 }
