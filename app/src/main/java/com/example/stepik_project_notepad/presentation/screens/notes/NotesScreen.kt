de6702b1d4eb18203854a8127bf767bbfd761e29
Mark <markdoroshko@yandex.ru>
2025-11-21 16:54:08 +0300

    NotesScreen UI. Часть 2.

--- app/src/main/java/com/example/stepik_project_notepad/presentation/screens/notes/NotesScreen.kt
+++ app/src/main/java/com/example/stepik_project_notepad/presentation/screens/notes/NotesScreen.kt
@@ -1,15 +1,19 @@
 package com.example.stepik_project_notepad.presentation.screens.notes
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.border
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -17,6 +21,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -29,6 +34,8 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.stepik_project_notepad.domain.Note
 import com.example.stepik_project_notepad.presentation.ui.theme.Green
+import com.example.stepik_project_notepad.presentation.ui.theme.OtherNotesColors
+import com.example.stepik_project_notepad.presentation.ui.theme.PinnedNotesColors
 import com.example.stepik_project_notepad.presentation.ui.theme.Yellow200
 
 @Composable
@@ -38,59 +45,86 @@ fun NotesScreen(
 ) {
     val state by viewModel.state.collectAsState()
 
-//    val scrollState = remember { ScrollState(0) }
-
     LazyColumn(
         modifier = modifier
-            .padding(top = 48.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+            .padding(top = 48.dp)
     ) {
         item {
-            Title(text = "All Notes")
+            Title(
+                modifier = Modifier.padding(horizontal = 24.dp),
+                text = "All Notes"
+            )
+        }
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
         }
         item {
             SearchBar(
-                query = state.query
-            ) { viewModel.processCommand(NotesCommand.InputSearchQuery(it)) }
+                modifier = Modifier.padding(horizontal = 24.dp),
+                query = state.query,
+                onQueryChange = { viewModel.processCommand(NotesCommand.InputSearchQuery(it)) }
+            )
         }
         item {
-            Subtitle(text = "Pinned")
+            Spacer(modifier = Modifier.height(24.dp))
+        }
+        item {
+            Subtitle(
+                modifier = Modifier.padding(horizontal = 24.dp),
+                text = "Pinned"
+            )
+        }
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
         }
         item {
             LazyRow(
                 modifier = Modifier
                     .fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(horizontal = 24.dp)
             ) {
-                items(
+                itemsIndexed(
                     items = state.pinnedNotes,
-                    key = { it.id }
-                ) { note ->
+                    key = { _, note -> note.id }
+                ) { index, note ->
                     NoteCard(
                         note = note,
                         onNoteClick = { viewModel.processCommand(NotesCommand.EditNote(it)) },
                         onLongClick = { viewModel.processCommand(NotesCommand.SwitchPinnedStatus(it.id)) },
                         onDoubleClick = { viewModel.processCommand(NotesCommand.DeleteNote(it.id)) },
-                        backgroundColor = Yellow200
+                        backgroundColor = PinnedNotesColors[index % PinnedNotesColors.size]
                     )
                 }
             }
         }
         item {
-            Subtitle(text = "Others")
+            Spacer(modifier = Modifier.height(24.dp))
         }
-        items(
+        item {
+            Subtitle(
+                modifier = Modifier.padding(horizontal = 24.dp),
+                text = "Others"
+            )
+        }
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        itemsIndexed(
             items = state.otherNotes,
-            key = { it.id }
-        ) { note ->
+            key = { _, note -> note.id }
+        ) { index, note ->
             NoteCard(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp),
                 note = note,
                 onNoteClick = { viewModel.processCommand(NotesCommand.EditNote(it)) },
                 onLongClick = { viewModel.processCommand(NotesCommand.SwitchPinnedStatus(it.id)) },
                 onDoubleClick = { viewModel.processCommand(NotesCommand.DeleteNote(it.id)) },
-                backgroundColor = Green
+                backgroundColor = OtherNotesColors[index % OtherNotesColors.size]
             )
+            Spacer(modifier = Modifier.height(8.dp))
         }
     }
 }
@@ -116,7 +150,13 @@ private fun SearchBar(
     onQueryChange: (String) -> Unit
 ) {
     TextField(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth()
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                shape = RoundedCornerShape(10.dp)
+            ),
         value = query,
         onValueChange = onQueryChange,
         placeholder = {
@@ -126,10 +166,17 @@ private fun SearchBar(
                 color = MaterialTheme.colorScheme.onSurfaceVariant
             )
         },
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = MaterialTheme.colorScheme.surface,
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        ),
         leadingIcon = {
             Icon(
                 imageVector = Icons.Default.Search,
-                contentDescription = "Search Notes"
+                contentDescription = "Search Notes",
+                tint = MaterialTheme.colorScheme.onSurface
             )
         },
         shape = RoundedCornerShape(10.dp)
@@ -167,18 +214,21 @@ fun NoteCard(
                 onClick = { onNoteClick(note) },
                 onLongClick = { onLongClick(note) },
                 onDoubleClick = { onDoubleClick(note) }
-            ),
+            )
+            .padding(16.dp),
     ) {
         Text(
             text = note.title,
             fontSize = 14.sp,
             color = MaterialTheme.colorScheme.onSurface
         )
+        Spacer(modifier = Modifier.height(8.dp))
         Text(
             text = note.updatedAt.toString(),
             fontSize = 12.sp,
             color = MaterialTheme.colorScheme.onSurfaceVariant
         )
+        Spacer(modifier = Modifier.height(24.dp))
         Text(
             text = note.content,
             fontSize = 16.sp,
